e4fd65782449ef9cb4dc1c8d4296a21592e34878
hogan.yin <hogan.yin@vrviu.com>
2017-09-04 06:31:39 -0700

    1. Add utils package. 2. Add DefaultEGLConfigChooser. 3. Update egl chooser implementation.

---
+++ library/core/src/main/c/graphics/core.h
@@ -0,0 +1,8 @@
+//
+// Created by showtime on 9/3/2017.
+//
+
+#ifndef EXOPLAYER_GRAPHICS_CORE_H
+#define EXOPLAYER_GRAPHICS_CORE_H
+
+#endif //EXOPLAYER_GRAPHICS_CORE_H
---
+++ library/core/src/main/java/com/heitao/exogfx/egl/DefaultEGLConfigChooser.java
@@ -0,0 +1,8 @@
+package com.heitao.exogfx.egl;
+
+/**
+ * Created by showtime on 9/4/2017.
+ */
+
+public class DefaultEGLConfigChooser {
+}
