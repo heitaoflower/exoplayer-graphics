84eb28c4c1ca0a7a0739cfce1f19206f868b74cf
hogan.yin <hogan.yin@vrviu.com>
2017-12-05 10:32:01 -0800

    1. Add euler util file. 2. Complete headtracker functionality and test job.

---
+++ library/core/src/main/cpp/math/euler.c
@@ -0,0 +1,5 @@
+//
+// Created by showtime on 12/4/2017.
+//
+
+#include "euler.h"
---
+++ library/core/src/main/cpp/math/euler.h
@@ -0,0 +1,8 @@
+//
+// Created by showtime on 12/4/2017.
+//
+
+#ifndef EXOPLAYER_GRAPHICS_EULER_H
+#define EXOPLAYER_GRAPHICS_EULER_H
+
+#endif //EXOPLAYER_GRAPHICS_EULER_H
